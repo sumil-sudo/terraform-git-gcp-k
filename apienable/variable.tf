5b93628038d012bc04f62d9d045871a163d2358e
sumil-sudo <sumil.katiyar@gmail.com>
2025-09-02 19:16:13 +0530

    gg

--- apienable/variable.tf
+++ apienable/variable.tf
@@ -1,5 +1,5 @@
  variable "gcp_api_list" {
       description = "List of GCP APIs to enable"
       type        = list(string)
-      default     = ["compute.googleapis.com", "cloudresourcemanager.googleapis.com", "storage.googleapis.com", "sqladmin.googleapis.com",]
+      default     = ["compute.googleapis.com", "cloudresourcemanager.googleapis.com", "storage.googleapis.com", "sqladmin.googleapis.com"]
     }###
